509e8101795d6a52b9a3a0eea5113252e21ae585
ayushach007 <ayushach007@gmail.com>
2025-11-19 15:04:27 +0545

    created dimension model for users

---
+++ dbt_modelling/models/dimension/dim_users.sql
@@ -0,0 +1,17 @@
+with dim_users as (
+    select 
+        {{ adapter.quote("user_id") }},
+        concat({{ adapter.quote("first_name") }}, ' ', {{ adapter.quote("last_name") }}) as {{ adapter.quote("full_name") }},
+        {{ adapter.quote("email") }},
+        {{ adapter.quote("signup_date") }},
+        {{ adapter.quote("referral_source") }},
+        {{ adapter.quote("plan_id") }},
+        {{ adapter.quote("region") }},
+        {{ adapter.quote("valid_from") }},
+        {{ adapter.quote("valid_to") }}
+
+    from {{ ref('stg_users') }}
+)
+
+select * from dim_users
+limit 10
