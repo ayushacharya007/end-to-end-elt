dfac369af73730114cf9b3785a34f44812418849
ayushach007 <ayushach007@gmail.com>
2025-12-03 12:40:03 +0545

    feat(models): Update fct_usages model to include plan_id and refine session count calculation

--- dbt_modelling/models/fact/fct_usages.sql
+++ dbt_modelling/models/fact/fct_usages.sql
@@ -2,15 +2,14 @@ with fct_usages as(
     select 
         usage_date,
         user_id,
+        plan_id,
         sum(active_minutes) as total_active_minutes,
         sum(api_calls) as total_api_calls,
         sum(actions_performed) as total_actions_performed,
-        sum(storage_used_mb) as total_storage_used_mb,
-        count(user_id) as session_count
+        round(sum(storage_used_mb), 2) as total_storage_used_mb,
+        count(*) as session_count
     from {{ ref('dim_usages') }}
-    group by 
-        usage_date,
-        user_id
+    group by 1, 2, 3
 )
 
 select * from fct_usages
