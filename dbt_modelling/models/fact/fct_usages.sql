d52b7069e6d8782538f4b3f4e9f4d1aea1bac335
ayushach007 <ayushach007@gmail.com>
2025-12-03 10:36:40 +0545

    feat(models): Refactor fct_usages model to aggregate usage data and update schema definitions

--- dbt_modelling/models/fact/fct_usages.sql
+++ dbt_modelling/models/fact/fct_usages.sql
@@ -1,20 +1,17 @@
 with fct_usages as(
     select 
-        du.usage_date,
-        du.user_id,
-        du.actions_performed as total_actions,
-        du.api_calls as total_api_calls,
-        du.storage_used_mb as total_storage_used_mb,
-        du.active_minutes as total_active_minutes,
-        {# count of occurance of each user on same date #}
-        count(du.user_id) as session_count
-
-    from {{ ref('dim_usages') }} as du
-
+        usage_date,
+        user_id,
+        sum(active_minutes) as total_active_minutes,
+        sum(api_calls) as total_api_calls,
+        sum(actions_performed) as total_actions_performed,
+        sum(storage_used_mb) as total_storage_used_mb,
+        count(user_id) as session_count
+    from {{ ref('dim_usages') }}
     group by 
-        du.usage_date,
-        du.user_id
+        usage_date,
+        user_id
 )
 
 select * from fct_usages
-limit 10
+
