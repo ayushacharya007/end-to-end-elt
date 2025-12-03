415c3b7d328cee689547d1b0a19af2382d1d80a0
ayushach007 <ayushach007@gmail.com>
2025-12-03 23:16:30 +0545

    feat: Add Pydantic models for User, Subscription, and Usage; update database configuration and API response structure

--- fastapi/config/config.py
+++ fastapi/config/config.py
@@ -11,13 +11,17 @@ load_dotenv(dotenv_path="../.env")  # provide correct path to your .env file
 
 # Environment variables
 RAILWAY_DATABASE_URL = os.environ.get("RAILWAY_DATABASE_URL")
+LOCAL_DATABASE_URL = os.environ.get("LOCAL_DATABASE_URL")
 
 if not RAILWAY_DATABASE_URL:
     raise ValueError("RAILWAY_DATABASE_URL environment variable is not set")
 
+if not LOCAL_DATABASE_URL:
+    raise ValueError("LOCAL_DATABASE_URL environment variable is not set")
+
 # Create SQLAlchemy engine and session for Railway database
 try:
-    engine = create_engine(RAILWAY_DATABASE_URL)  # Use Railway database for engine
+    engine = create_engine(LOCAL_DATABASE_URL)  # Use Railway database for engine
     # Create a configured "Session" class
     session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 except Exception as e:
